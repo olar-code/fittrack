ad58d9e708bf796c463bca5207799d877a9fabcd
olar-code <30886306+olar-code@users.noreply.github.com>
2026-02-27 23:34:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,28 @@
-const CACHE_NAME = "fittracker-v2";
+const CACHE_NAME = "fittracker-v3";
 
 const urlsToCache = [
-  "/fittrack/",
-  "/fittrack/index.html",
-  "/fittrack/style.css",
-  "/fittrack/app.js",
-  "/fittrack/manifest.json",
-  "/fittrack/icon-192.png",
-  "/fittrack/icon-512.png",
-  "/fittrack/service-worker.js"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
